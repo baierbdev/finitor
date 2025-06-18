53f1317618849c3c04ba6d0a4569ae8db97e6ecd
Bruno Baier <brunobaier3@gmail.com>
2025-06-18 16:20:28 -0300

    Estrutura do projeto inicial

---
+++ finitor-server/src/test/java/com/finitor/FinitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
